d9fc0960864fa05ebba3c1e12c742832091fd8da
Shah Habibul Imran <imran15-10627@diu.edu.bd>
2020-12-14 04:39:18 +0600

    fix: About and report section

--- app/src/main/java/com/expensexpert/expensexpert/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/expensexpert/expensexpert/ui/slideshow/SlideshowFragment.java
@@ -23,7 +23,6 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_report);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
